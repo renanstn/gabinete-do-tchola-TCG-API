594054c4dfdb43b831ba20d6ee3a219357ae3876
Renan Santana <renan_ota@hotmail.com>
2025-01-31 18:53:20 -0300

    Improve repositories

--- card_game/adapters/repositories/sqlite.py
+++ card_game/adapters/repositories/sqlite.py
@@ -1,6 +1,21 @@
+from sqlalchemy.orm import Session
+
 from card_game.adapters.repositories.base import BaseRepository
+from card_game.adapters.repositories.models.game import Game
 
 
 class SqliteRepository(BaseRepository):
-    def save_game_state(self):
-        pass
+    def __init__(self, session: Session):
+        self.session = session
+
+    def save_game_state(self, game: Game):
+        existing_game = (
+            self.session.query(Game).filter(Game.id == game.id).first()
+        )
+        if existing_game:
+            existing_game.winner = game.winner
+            existing_game.turn = game.turn
+            existing_game.active = game.active
+        else:
+            self.session.add(game)
+        self.session.commit()
