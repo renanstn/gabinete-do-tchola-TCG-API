fdfd9bde5dabe0676260d08790df039d91fc5df7
Renan <renan_ota@hotmail.com>
2025-02-26 19:11:55 -0300

    Add Player model integration to game state in test_db function

--- card_game/app.py
+++ card_game/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from adapters.repositories.factory import get_repository
 from adapters.repositories.models.game import Game
+from adapters.repositories.models.player import Player
 
 app = Flask(__name__)
 
@@ -18,5 +19,8 @@ def test_db():
     está funcionando.
     """
     repository = get_repository()
-    game = Game()
+    player_01 = Player(name="Foo")
+    player_02 = Player(name="Bar")
+    game = Game(players=[player_01, player_02])
     repository.save_game_state(game)
+    return "Done!"
