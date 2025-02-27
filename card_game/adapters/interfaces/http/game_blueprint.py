a279432a242026388749c88704f8644f9b8eb8dc
Renan <renan_ota@hotmail.com>
2025-02-26 21:40:46 -0300

    Add check_turn endpoint to game blueprint for turn validation

--- card_game/adapters/interfaces/http/game_blueprint.py
+++ card_game/adapters/interfaces/http/game_blueprint.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from application.game_service import GameService
 
 game_blueprint = Blueprint("game", __name__, url_prefix="/game")
 
@@ -6,6 +7,16 @@ game_blueprint = Blueprint("game", __name__, url_prefix="/game")
 @game_blueprint.route("/hello")
 def say_hi():
     return "Hi from games!"
+
+
+@game_blueprint.route("/<game_id>/check_turn")
+def check_turn(game_id: int):
+    """
+    curl -X GET "http://localhost:5000/game/1/check_turn"
+    """
+    print(game_id)
+    GameService.check_turn(game_id)
+    return "todo"
 
 
 @game_blueprint.route("/start", methods=["POST"])
